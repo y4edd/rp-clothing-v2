c1e9ddb115a7dd9aa122359398bf4c42e21b6c79
y4edd <yaeyuu0116@icloud.com>
2025-08-03 17:20:43 +0900

    docs: swaggerのセットアップ

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+
+  const config = new DocumentBuilder()
+    .setTitle('サンプルAPI')
+    .setDescription('サンプルAPIの紹介')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = process.env.PORT ?? 8080;
+  await app.listen(port);
+
+  logger.log(`API URL is here: http://localhost:${port}`);
+  logger.log(`Docs is here: http://localhost:${port}/docs`);
 }
 bootstrap();
